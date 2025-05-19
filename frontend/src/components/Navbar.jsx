3de86f7fc3deaa6a56f57f37d6fde928a12edeb6
Jeremy Suriel <73365279+COPY0096@users.noreply.github.com>
2025-05-18 23:17:48 -0700

    Navegacion entre paginas en proceso #1

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import { Container, Flex } from '@chakra-ui/react'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <Container maxW={"1140px"} px={4}>
+        <Flex
+        h={16}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+        flexDir={{
+            base: "column", 
+            sm: "row"
+            }}
+        >
+            <Text
+            fontSize={{base:"22", sm: "28"}}
+            fontWeight={"Bold"}
+            textTransform={"uppercase"}
+            textAlign={"center"}
+            bgGradient={"linear(to-r, cyan.400, blue.500)"}
+            bgClip={"text"}
+            >
+               <Link to={"/"}>Prduct Store 🛒</Link>
+            </Text>
+            
+            
+        </Flex>
+    </Container>
+  )
+}
+
+export default Navbar
