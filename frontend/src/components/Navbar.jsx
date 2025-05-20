dba57fd9f11b8d072b0fc79d2ea5b53b89d3e2c5
Jeremy Suriel <73365279+COPY0096@users.noreply.github.com>
2025-05-19 18:00:31 -0700

    light/night modes paginas en proceso #2

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,14 @@
-import { Container, Flex } from '@chakra-ui/react'
+import { Button, Container, Flex, HStack, Text, useColorModeValue } from '@chakra-ui/react'
+import { PlusSquareIcon } from '@chakra-ui/icons'
+import { useColorMode } from '@chakra-ui/react'
+import { IoMoon, IoSunny } from 'react-icons/io5'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <Container maxW={"1140px"} px={4}>
+    <Container maxW={"1140px"} px={4} bg={useColorModeValue("gray.100", "gray.900")}>
         <Flex
         h={16}
         alignItems={"center"}
@@ -22,9 +26,19 @@ const Navbar = () => {
             bgGradient={"linear(to-r, cyan.400, blue.500)"}
             bgClip={"text"}
             >
-               <Link to={"/"}>Prduct Store 🛒</Link>
+              <Link to={"/"}>Product Store 🛒</Link>
             </Text>
             
+            <HStack spacing={2} alignItems={"center"}> 
+              <Link to={"/create"}>
+              <Button>
+                <PlusSquareIcon fontSize={20} />
+              </Button>
+              </Link>
+              <Button onClick={toggleColorMode}>
+                {colorMode === "light" ? <IoMoon/> : <IoSunny size={20} />}
+              </Button>
+            </HStack>
             
         </Flex>
     </Container>
